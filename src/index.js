b5c95e3ac1ba1415e25c575f58962af04b0931a1
hanxipeng <hanxipeng@bytedance.com>
2017-07-25 10:40:19 +0800

    feat: 现在可以配置多个dataFile
    Change-Id: Ia71541d2c5b66dc6894c4129d8bf6ed1b33d0be3

--- src/index.js
+++ src/index.js
@@ -13,18 +13,18 @@ const generateTemplatesByComponentsObj = require('./generateTemplatesByComponent
 const mapFnStrToFunction = require('./mapFnStrToFunction');
 
 function mergeComponentData (componentDataArray) {
-  let result = {};
-  componentDataArray.map(componentsData => {
-    let componentNames = Object.keys(componentsData);
-    componentNames.map(componentName => {
-      if (result[componentName]) {
-        result[componentName].push(...componentsData[componentName]);
-      } else {
-        result[componentName] = componentsData[componentName];
-      }
+    let result = {};
+    componentDataArray.map(componentsData => {
+        let componentNames = Object.keys(componentsData);
+        componentNames.map(componentName => {
+            if (result[componentName]) {
+                result[componentName].push(...componentsData[componentName]);
+            } else {
+                result[componentName] = componentsData[componentName];
+            }
+        });
     });
-  });
-  return result;
+    return result;
 }
 
 (async function (params) {
@@ -35,6 +35,8 @@ function mergeComponentData (componentDataArray) {
             '-c,  --config <path>',
             'js配置文件, 文件包含react component信息或者react component-data信息'
         )
+        .option('-o, --output <outfilename>', '保存文件的名称')
+        .option('-u, --url <url>', '想要获取数据的url')
         .parse(process.argv);
 
     if (!process.config) {
@@ -48,48 +50,59 @@ function mergeComponentData (componentDataArray) {
 
     const urls = config.urls || [];
     const Components = config.Components || {};
-    const dataFile = config.dataFile || '';
+    const dataFiles = config.dataFiles || [];
 
-    if (urls.length === 0 && !dataFile) {
+    if (urls.length === 0 && dataFiles.length === 0) {
         console.error('urls/dataFile必须配置其中至少一项');
         process.exit(0);
     }
 
     let componentsData;
-    if (dataFile) {
-        let absoluteDataFile = path.join(path.dirname(configPath), dataFile);
-        try {
-            componentsData = require(absoluteDataFile);
-        } catch (err) {
-            console.error(err);
-            process.exit(0);
+    if (dataFiles.length > 0) {
+        let dataContainer = [];
+        for (let i = 0; i < dataFiles.length; i++) {
+            let absoluteDataFile = path.join(
+                path.dirname(configPath),
+                dataFiles[i]
+            );
+            try {
+                let tmpData = require(absoluteDataFile);
+                dataContainer.push(tmpData);
+            } catch (err) {
+                console.error(err);
+            }
         }
+        componentsData = mergeComponentData(dataContainer);
     } else {
         let dataContainer = [];
         for (let i = 0; i < urls.length; i++) {
-          let tmpComponentData = await fetchReactProps(urls[i]);
-          dataContainer.push(JSON.parse(tmpComponentData));
+            let tmpComponentData = await fetchReactProps(urls[i]);
+            dataContainer.push(JSON.parse(tmpComponentData));
         }
         componentsData = mergeComponentData(dataContainer);
         if (JSON.stringify(componentsData) === '{}') {
-          console.log('从url地址获取的数据为空, 退出');
-          process.exit(0);
+            console.log('从url地址获取的数据为空, 退出');
+            process.exit(0);
         }
         savePropsData(JSON.stringify(componentsData), configDir);
     }
 
-    if ( !componentsData || JSON.stringify(componentsData) === '{}') {
-      console.log('获取到的数据为空, 退出');
-      process.exit(0);
+    if (!componentsData || JSON.stringify(componentsData) === '{}') {
+        console.log('获取到的数据为空, 退出');
+        process.exit(0);
     }
 
     if (JSON.stringify(Components) !== '{}') {
-      const componentsObj = getComponentsObject(Components, componentsData);
-      const componentsObjWithTest = generateTemplatesByComponentsObj(componentsObj);
-      componentsObjWithTest.map(componentsObj => {
-        let name = componentsObj.name;
-        saveTestFile(`${configDir}/${name}.test.js`, componentsObj.testTemplate);
-      });
+        const componentsObj = getComponentsObject(Components, componentsData);
+        const componentsObjWithTest = generateTemplatesByComponentsObj(
+            componentsObj
+        );
+        componentsObjWithTest.map(componentsObj => {
+            let name = componentsObj.name;
+            saveTestFile(
+                `${configDir}/${name}.test.js`,
+                componentsObj.testTemplate
+            );
+        });
     }
-
 })();
